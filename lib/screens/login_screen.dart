2268d635b84e7f66fe7de6846b29c5314e25e1ba
Ahmad <muahmad710@gmail.com>
2022-11-12 03:51:47 +0500

    Login Screen UI completed

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:pokedoke/constants/colors.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _loginFormKey = GlobalKey<FormBuilderState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: scaffoldBackgroundColor,
+      body: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 70,
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                "Login",
+                style: TextStyle(
+                    color: secondaryColor,
+                    fontSize: 32,
+                    fontWeight: FontWeight.w700),
+              ),
+            ),
+            const SizedBox(
+              height: 60,
+            ),
+            FormBuilder(
+                key: _loginFormKey,
+                child: Column(
+                  children: [
+                    FormBuilderTextField(
+                      name: 'emailField',
+                      cursorColor: secondaryColor,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        hintText: 'Enter your email',
+                        labelStyle: TextStyle(
+                          color: secondaryColor,
+                          fontSize: 16,
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: secondaryColor),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: secondaryColor),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    FormBuilderTextField(
+                      name: 'passwordField',
+                      cursorColor: secondaryColor,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        hintText: 'Enter your password',
+                        labelStyle: TextStyle(
+                          color: secondaryColor,
+                          fontSize: 16,
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: secondaryColor),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: secondaryColor),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        child: ElevatedButton(
+                            onPressed: () {},
+                            style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                        secondaryColor)),
+                            child: const Text(
+                              "Login",
+                              style: TextStyle(fontSize: 16),
+                            ))),
+                  ],
+                )),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.42,
+            ),
+            GestureDetector(
+              onTap: () {
+                
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  Text("Don't have an account? "),
+                  Text("Sign up!", style: TextStyle(
+                    decoration: TextDecoration.underline
+                  ),)
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
