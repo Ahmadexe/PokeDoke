89af25ef255b7f08da2f7039b81c24cafec8c73d
Ahmad <muahmad710@gmail.com>
2022-11-13 01:44:56 +0500

    Poke Api fetched

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,9 +2,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pokedoke/api/pokemon_api_methods.dart';
 import 'package:pokedoke/blocs/user%20bloc/bloc/user_bloc.dart';
 import 'package:pokedoke/constants/colors.dart';
 import 'package:pokedoke/database/cloud/firestore_methods.dart';
+import 'package:pokedoke/models/pokemons.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -15,11 +17,19 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  late Pokemons pokemons;
+
   @override
   void initState() {
+    getData();
     super.initState();
   }
 
+  getData() async {
+    pokemons = await ApiService().getPokemons();
+    print(pokemons.pokemon);
+  }
 
   @override
   Widget build(BuildContext context) {
