af90e5e0e9a71ade0341b4f3f75f6eefeeb1ca05
Ahmad <muahmad710@gmail.com>
2022-11-13 03:26:53 +0500

    Auth module complete, home screen UI partial

--- lib/widgets/poke_card.dart
+++ lib/widgets/poke_card.dart
@@ -4,33 +4,48 @@ import 'package:pokedoke/models/pokemons.dart';
 
 class PokeCard extends StatelessWidget {
   final Pokemon pokemon;
-  const PokeCard({required this.pokemon,super.key});
+  const PokeCard({required this.pokemon, super.key});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      color:Colors.greenAccent,
+      color: pokemon.type![0] == "Grass"
+          ? Colors.greenAccent
+          : pokemon.type![0] == "Fire"
+              ? Colors.redAccent
+              : pokemon.type![0] == "Water"
+                  ? Colors.blue
+                  : pokemon.type![0] == "Poison"
+                      ? Colors.deepPurpleAccent
+                      : pokemon.type![0] == "Electric"
+                          ? Colors.amber
+                          : pokemon.type![0] == "Rock"
+                              ? Colors.grey
+                              : pokemon.type![0] == "Ground"
+                                  ? Colors.brown
+                                  : pokemon.type![0] == "Psychic"
+                                      ? Colors.indigo
+                                      : pokemon.type![0] == "Fighting"
+                                          ? Colors.orange
+                                          : pokemon.type![0] == "Bug"
+                                              ? Colors.lightGreenAccent
+                                              : pokemon.type![0] == "Ghost"
+                                                  ? Colors.deepPurple
+                                                  : pokemon.type![0] == "Normal"
+                                                      ? Colors.black26
+                                                      : Colors.pink,
       child: Stack(
         children: [
+          Positioned(top: 30, left: 10, child: Text(pokemon.name!)),
+          Positioned(top: 45, left: 10, child: Text(pokemon.type![0])),
           Positioned(
-            top: 30,
-            left: 10,
-            child: Text(pokemon.name!) 
-          ),
-          Positioned(
-            top: 45,
-            left: 10,
-            child: Text(pokemon.type![0]) 
-          ),
-          Positioned(
-            bottom: 5,
-            right: -10,
-            child: CachedNetworkImage(
-              imageUrl: pokemon.img!,
-              height: 100,
-              fit: BoxFit.fitHeight,
-            )
-          )
+              bottom: 5,
+              right: -10,
+              child: CachedNetworkImage(
+                imageUrl: pokemon.img!,
+                height: 100,
+                fit: BoxFit.fitHeight,
+              ))
         ],
       ),
     );
